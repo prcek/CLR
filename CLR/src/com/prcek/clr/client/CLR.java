94bb262203201c7f69d42f582171a40601bdfb05
Prcek <tomas.hluchan@gmail.com>
2012-01-04 11:00:28 +0100

    pridano GXT

--- CLR/src/com/prcek/clr/client/CLR.java
+++ CLR/src/com/prcek/clr/client/CLR.java
@@ -16,6 +16,7 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.extjs.gxt.ui.client.widget.DatePicker;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -39,6 +40,7 @@ public class CLR implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		final DatePicker datePicker = new DatePicker();
 		final Button sendButton = new Button("Send");
 		final TextBox nameField = new TextBox();
 		nameField.setText("GWT User");
@@ -49,6 +51,7 @@ public class CLR implements EntryPoint {
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
+		RootPanel.get("datePickerContainer").add(datePicker);
 		RootPanel.get("nameFieldContainer").add(nameField);
 		RootPanel.get("sendButtonContainer").add(sendButton);
 		RootPanel.get("errorLabelContainer").add(errorLabel);
